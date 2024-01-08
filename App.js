eb2de9bbce36983f6677b5925cc45ea72a663ee2
netcoretiger <155702693+netcoretiger@users.noreply.github.com>
2024-01-08 02:29:46 -0800

    Add Double gesture

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 import Button from './components/Button';
 import CircleButton from './components/CircleButton';
@@ -10,6 +11,7 @@ import ImageViewer from './components/ImageViewer';
 import EmojiPicker from "./components/EmojiPicker";
 import EmojiList from './components/EmojiList';
 import EmojiSticker from './components/EmojiSticker';
+
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
@@ -51,7 +53,7 @@ export default function App() {
 
 
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer
           placeholderImageSource={PlaceholderImage}
@@ -77,7 +79,7 @@ export default function App() {
         <EmojiList onSelect={setPickedEmoji} onCloseModal={onModalClose} />
       </EmojiPicker>
       <StatusBar style="auto" />
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
--- components/EmojiSticker.js
+++ components/EmojiSticker.js
@@ -1,13 +1,33 @@
-import { View, Image } from 'react-native';
+import { View } from 'react-native';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import Animated, { useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
 
 export default function EmojiSticker({ imageSize, stickerSource }) {
-  return (
-    <View style={{ top: -350 }}>
-      <Image
-        source={stickerSource}
-        resizeMode="contain"
-        style={{ width: imageSize, height: imageSize }}
-      />
-    </View>
-  );
+    const scaleImage = useSharedValue(imageSize);
+
+    const doubleTap = Gesture.Tap()
+        .numberOfTaps(2)
+        .onStart(() => {
+            if (scaleImage.value !== imageSize * 2) {
+                scaleImage.value = scaleImage.value * 2;
+            }
+        });
+
+    const imageStyle = useAnimatedStyle(() => {
+        return {
+            width: withSpring(scaleImage.value),
+            height: withSpring(scaleImage.value),
+        };
+    });
+    return (
+        <View style={{ top: -350 }}>
+            <GestureDetector gesture={doubleTap}>
+                <Animated.Image
+                    source={stickerSource}
+                    resizeMode="contain"
+                    style={[imageStyle, { width: imageSize, height: imageSize }]}
+                />
+            </GestureDetector>
+        </View>
+    );
 }
